bca7c5480cc822f71dd6b22d9f5b2eb35649d16e
kevinly618 <kevinly618@gmail.com>
2020-08-27 12:07:21 -0500

    QA New Login Button

--- js/scripts.js
+++ js/scripts.js
@@ -8,64 +8,81 @@
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-9EQZKYSPO8MH-2-3");
 
+function loginA(){
+  var a = "kevinly618_Stage@gmail.com"
 
-// LOGIN PORTION AND PX IDENTIFY CODE
-function login() {
-  //var a = document.form.usermail.value;
-  var a = document.getElementById("usermail").value;
-  var b;
-  var c;
-
-  if (a != "") {
-    if (a == "kevinly618_QA@gmail.com") {
-      alert("valid User");
-      b = a.substr(0, 8);
-
-
-//PX IDENTIFICAITON PORTION for Known Vistors
-      aptrinsic("identify",
-        {
-        //User Fields
-          "id": a, // Required for logged in app users
-          "email": a,
-          "firstName": "Kevin",
-          "lastName": "Ly",
-        },
-        {
-        //Account Fields
-          "id":"1P02JCQSL9U0L1CYDINVA002GDEEI2LK32JS", //Required
-          "name":"Betty Nguyen Corporation",
-       });
-
-    window.location = "index.html"; // Keep your index page (page you want to navigate after login)
-
-
-  } else if (a == "bsons_QA@gmail.com") {
-      alert("valid User");
-
-      b = a.substr(0, 8);
-      window.location = "index.html"; // Keep your index page (page you want to navigate after login)
-
-//PX IDENTIFICAITON PORTION for Known Vistors
-      aptrinsic("identify",
-        {
-        //User Fields
-          "id": a, // Required for logged in app users
-          "email": a,
-          "firstName": "Betty",
-          "lastName": "Corp",
-        },
-        {
-        //Account Fields
-          "id":"1P02JCQSL9U0L1CYDINVA002GDEEI2LK32JS", //Required
-          "name":"Burlington & Sons Corporation",
-       });
-
-
-    }
-    else
-      alert("Invalid User,enter kevinly618_QA@gmail.com or bsons_QA@gmail.com");
-  } else
-    alert("enter kevinly618_QA@gmail.com or bsons_QA@gmail.com");
-  return b;
+        aptrinsic("identify",
+          {
+          //User Fields
+            "id": a, // Required for logged in app users
+            "email": a,
+            "firstName": "Kevin",
+            "lastName": "Ly",
+          },
+          {
+          //Account Fields
+            "id":"1P02JCQSL9U0L1CYDINVA002GDEEI2LK32JS_QA", //Required
+            "name":"Betty Nguyen Corporation_QA",
+         });
+    console.log('LoginA function complete')
 }
+// // LOGIN PORTION AND PX IDENTIFY CODE
+// function login() {
+//   //var a = document.form.usermail.value;
+//   var a = document.getElementById("usermail").value;
+//   var b;
+//   var c;
+//
+//   if (a != "") {
+//     if (a == "kevinly618_QA@gmail.com") {
+//       alert("valid User");
+//       b = a.substr(0, 8);
+//
+//
+// //PX IDENTIFICAITON PORTION for Known Vistors
+//       aptrinsic("identify",
+//         {
+//         //User Fields
+//           "id": a, // Required for logged in app users
+//           "email": a,
+//           "firstName": "Kevin",
+//           "lastName": "Ly",
+//         },
+//         {
+//         //Account Fields
+//           "id":"1P02JCQSL9U0L1CYDINVA002GDEEI2LK32JS", //Required
+//           "name":"Betty Nguyen Corporation",
+//        });
+//
+//     window.location = "index.html"; // Keep your index page (page you want to navigate after login)
+//
+//
+//   } else if (a == "bsons_QA@gmail.com") {
+//       alert("valid User");
+//
+//       b = a.substr(0, 8);
+//       window.location = "index.html"; // Keep your index page (page you want to navigate after login)
+//
+// //PX IDENTIFICAITON PORTION for Known Vistors
+//       aptrinsic("identify",
+//         {
+//         //User Fields
+//           "id": a, // Required for logged in app users
+//           "email": a,
+//           "firstName": "Betty",
+//           "lastName": "Corp",
+//         },
+//         {
+//         //Account Fields
+//           "id":"1P02JCQSL9U0L1CYDINVA002GDEEI2LK32JS", //Required
+//           "name":"Burlington & Sons Corporation",
+//        });
+//
+//
+//     }
+//     else
+//       alert("Invalid User,enter kevinly618_QA@gmail.com or bsons_QA@gmail.com");
+//   } else
+//     alert("enter kevinly618_QA@gmail.com or bsons_QA@gmail.com");
+//   return b;
+// }
